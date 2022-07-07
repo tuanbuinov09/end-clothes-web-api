ea376ed29b0562d2d5e8f56077bb8ec98dd5b7eb
Tuan EnderMan <t01684599820@gmail.com>
2022-07-08 00:52:50 +0700

    allow cors

--- ClothingWebAPI/Startup.cs
+++ ClothingWebAPI/Startup.cs
@@ -67,6 +67,11 @@ namespace ClothingWebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Use cors
+            app.UseCors(options => {
+                options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
