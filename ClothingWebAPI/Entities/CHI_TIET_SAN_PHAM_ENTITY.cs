08eb2bc8b28997296a017bf97c1e0b4eb32d8ca4
Tuan EnderMan <t01684599820@gmail.com>
2022-07-20 00:35:07 +0700

    cap nhat lay mot sp

---
+++ ClothingWebAPI/Entities/CHI_TIET_SAN_PHAM_ENTITY.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClothingWebAPI.Entities
+{
+    public partial class CHI_TIET_SAN_PHAM_ENTITY
+    {
+        public CHI_TIET_SAN_PHAM_ENTITY()
+        {
+          
+        }
+
+        public int MA_CT_SP { get; set; }
+        public string MA_SP { get; set; }
+        public string MA_SIZE { get; set; }
+        public string TEN_SIZE { get; set; }
+        public int? GIA { get; set; }
+        public int? SL_TON { get; set; }
+
+    }
+}
